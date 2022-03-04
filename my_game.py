0e05b61fe8d8370e7ad587e22426526a394e4351
tetianavinnik <92581380+tetianavinnik@users.noreply.github.com>
2022-03-04 10:28:51 +0200

    Update my_game.py

--- my_game.py
+++ my_game.py
@@ -1,5 +1,6 @@
 """The game classes"""
-
+import time
+import random
 
 class City:
     """
@@ -88,6 +89,102 @@ class BossCity(City):
     """
     def __init__(self, name: str, open='', points='', enemy='', friend='', item='', free=False) -> None:
         super().__init__(name, open, points, enemy, friend, item, free)
+
+
+    def boss_level_one(self):
+        print("""
+Вітаю! Залишилось звільнити останнє місто - Харків.
+Для цього тобі знадобиться вміння управляти літаком МіГ-29.
+
+Інструкція:
+
+Зараз ти знаходишся під обстрілом ворожого літака,
+тобі потрібно ухилятися від ракет, які ти бачиш на радарі.
+Як? - Потрібно ввести одну з чотирьох команд(праворуч, ліворуч, вгору, вниз).
+Будь уважним, час обмежений!
+Натисни Enter, коли будеш готовий""")
+        input('> ')
+        win = False
+        while win == False:
+            times = 0
+            breaks = 0
+            dec = ['праворуч', 'ліворуч', 'вгору', 'вниз']
+            shot = ['праворуч', 'ліворуч', 'згори', 'знизу']
+            win = False
+            while times != 5 or breaks != 3:
+                shoot = random.choice(shot)
+                print('Обережно! Обстріл {}'.format(shoot))
+                start = time.time()
+                decision = input('> ')
+                end = time.time()
+                if end-start > 6:
+                    breaks += 1
+                    print('Тебе підсмажили. Думай швидше!')
+                elif decision not in dec:
+                    breaks += 1
+                    print('Такої опції немає')
+                elif dec[shot.index(shoot)] == decision:
+                    breaks += 1
+                    print('Тебе підсмажили. Думай краще!')
+                else:
+                    times += 1
+                    print('Так тримати!')
+                if breaks == 3:
+                    print('Тебе збили! Спробуй ще раз.')
+                    input('> ')
+                elif times == 5:
+                    print('\nМолодець! Ти зміг втекти зпід обстрілу!\n')
+                    win = True
+                if win == True:
+                    break
+
+
+    def boss_level_two(self):
+        print("""
+Ти неймовірний! Залишилось зовсім трохи.
+
+Настав час надерти зад рашистам, адже вони виглядають набагато краще підсмаленими.
+Слідкуй за радаром і випускай ракети в напрямку ворожих літаків.
+Хай щастить!
+Натисни Enter, коли будеш готовий""")
+        input('> ')
+        win = False
+        while win == False:
+            times = 0
+            breaks = 0
+            dec = ['праворуч', 'ліворуч', 'вгору', 'вниз']
+            shot = ['праворуч', 'ліворуч', 'згори', 'знизу']
+            win = False
+            while times != 5 or breaks != 3:
+                shoot = random.choice(shot)
+                print('Увага! Літак {}'.format(shoot))
+                start = time.time()
+                decision = input('> ')
+                end = time.time()
+                if end-start > 6:
+                    breaks += 1
+                    print('Ти пропустив свій шанс. Думай швидше!')
+                elif decision not in dec:
+                    breaks += 1
+                    print('Такої опції немає')
+                elif dec[shot.index(shoot)] != decision:
+                    breaks += 1
+                    print('Ти впустив свій шанс. Думай краще!')
+                else:
+                    times += 1
+                    print('Так тримати!')
+                if breaks == 3:
+                    print('Ти пропустив 3 літаки підряд! Спробуй ще раз.')
+                    input('> ')
+                elif times == 5:
+                    print('\nЙухуу! Завдяки тобі жителі Харкова змогли насолодитися рашистським салютом')
+                    win = True
+                if win == True:
+                        break
+        print("""
+Ти звільнив Україну від окупантів!
+
+Слава Україні!""")
 
 
 class Person:
